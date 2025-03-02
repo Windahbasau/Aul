473da551730105ea08d1085fe15450b0a98a3e16
Windahbasau <125420698+Windahbasau@users.noreply.github.com>
2025-03-03 05:19:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "Qin" ]; then
+  if [ "$USER_TOKEN" = "Qinn" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke Qin${NC}"
